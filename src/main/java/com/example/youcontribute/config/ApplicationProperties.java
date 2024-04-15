fdaf0061d2a70512fba3cc77e325de9dbdaa033b
Agil Aghamirazyev <aqilzeka99@gmail.com>
2024-04-15 22:02:12 +0400

    Application preoperties

---
+++ src/main/java/com/example/youcontribute/config/ApplicationProperties.java
@@ -0,0 +1,16 @@
+package com.example.youcontribute.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "application")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ApplicationProperties {
+    private Integer importFrequency;
+}
